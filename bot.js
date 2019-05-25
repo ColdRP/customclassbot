bcf48ce6789652a08228cc2c22fd370dacd9e5b8
ColdRP <51008632+ColdRP@users.noreply.github.com>
2019-05-25 10:15:25 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
 client.on('ready', () => {
@@ -8,7 +7,10 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.content === '!customclass') {
-    msg.author.send("Hi! Before buying a Custom Class you should have read and agreed to the following terms: \n https://coldrpofficial.com/CustomClasses \n https://coldrpofficial.com/CustomClassesTOS")
+    msg.author.send("Hi! Before buying a Custom Class you should read this: https://coldrpofficial.com/CustomClasses \n If you are purchasing you also agree to these TOS : https://coldrpofficial.com/CustomClassesTOS \n If you have read both of these you agree to the TOS by purchasing a custom class, Message the Custom Class Manager if you are interested in buying a custom class, If they do not respond, message the Owner or someone such as the head admin.")
+  }
+  else if (msg.content === '!nib') {
+    msg.author.send("Hello")
   }
 });
 

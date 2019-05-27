01a078a8d58e2db2ef21069d175b9226acaea162
ColdRP <51008632+ColdRP@users.noreply.github.com>
2019-05-26 22:52:08 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,8 @@ client.on('message', msg => {
   if (msg.content === '!customclass') {
     msg.author.send("Hi! Before buying a Custom Class you should read this: https://coldrpofficial.com/CustomClasses \n If you are purchasing you also agree to these TOS : https://coldrpofficial.com/CustomClassesTOS \n If you have read both of these you agree to the TOS by purchasing a custom class, Message the Custom Class Manager if you are interested in buying a custom class, If they do not respond, message the Owner or someone such as the head admin.")
   }
-  else if (msg.content === '!nib') {
-    msg.author.send("Hello")
+  else if (msg.content === '!serverinfo') {
+    msg.author.send("Hi, Here is the information about the server \n Forums : https://coldrpofficial.com \n Store : https://coldrpofficial.com/Store \n MOTD : https://coldrpofficial.com/Motd \n I hope that is all you need!")
   }
 });
 
